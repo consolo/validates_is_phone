e7e21c8b1147485af750ecdb2082f8b3b274f8e8
Andrew Coleman <consolocoleman@gmail.com>
2012-01-03 15:31:24 -0600

    friendly style. send an extend rather than opening the class.

--- validates_is_phone.rb
+++ validates_is_phone.rb
@@ -1,29 +1,29 @@
 gem 'activerecord', '~> 2.3'
 
-module ActiveRecord
-  module Validations
-    module ClassMethods
-      def validates_is_phone(*attr_names)
-        configuration = { 
-          :required_message => "is required", 
-          :format_message => "must be formated XXXXXXXXXX (10/11 digits)", 
-          :required => false
-        }
-        configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
-        
-        validates_each(attr_names, configuration) do |record, attr_name, value|
-          if value.nil? or value == ''
-            if configuration[:required]
-              record.errors.add(attr_name, configuration[:required_message])
-            end
-          else
-            value.gsub!(/[^\d]/,'')
-            if value.length != 10 and value.length != 11
-              record.errors.add(attr_name, configuration[:format_message])
-            end
+module Consolo
+  module ValidatesIsPhone
+    def validates_is_phone(*attr_names)
+      configuration = { 
+        :required_message => "is required", 
+        :format_message => "must be formated XXXXXXXXXX (10/11 digits)", 
+        :required => false
+      }
+      configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
+      
+      validates_each(attr_names, configuration) do |record, attr_name, value|
+        if value.nil? or value == ''
+          if configuration[:required]
+            record.errors.add(attr_name, configuration[:required_message])
+          end
+        else
+          value.gsub!(/[^\d]/,'')
+          if value.length != 10 and value.length != 11
+            record.errors.add(attr_name, configuration[:format_message])
           end
         end
       end
     end
   end
 end
+
+ActiveRecord::Base.send :extend, Consolo::ValidatesIsPhone
